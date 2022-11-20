5877d8e699bb6b40d7dfdd0ef99e3b582ab085dc
Claudio Caniullan <claudio.caniullan@usach.cl>
2022-11-20 02:55:30 -0300

    modificar vista y html

--- tiendasport/tiendasport/views.py
+++ tiendasport/tiendasport/views.py
@@ -5,4 +5,13 @@ from django.shortcuts import render
 #    return HttpResponse('Hola')
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html',{
+        'message':'Listado de productos', 
+        'title':'Productos',
+        'products': [
+            {'title':'Playera', 'price':5, 'stock':True},
+            {'title':'Camisa', 'price':7, 'stock':True},
+            {'title':'Mochila', 'price':20, 'stock':False}
+            ]
+        }
+    )
