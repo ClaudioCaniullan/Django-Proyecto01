e6cdb61c4faea668166e42b4c1ef8bbbdffa369e
Claudio Caniullan <claudio.caniullan@usach.cl>
2022-12-15 23:38:48 -0300

    login usuario casi listo, falta validar campos

--- tiendasport/tiendasport/views.py
+++ tiendasport/tiendasport/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
+
 from django.contrib.auth import authenticate
 from django.contrib.auth import login, logout
+from django.contrib.auth.models import User
 from django.contrib import messages
 
 from .forms import RegistroForm
@@ -47,7 +49,21 @@ def logoutview(request):
 
 
 def registro(request):
-    form = RegistroForm()
+    form = RegistroForm(request.POST or None)
+    
+    if request.method == 'POST' and form.is_valid():
+        username = form.cleaned_data.get('username')
+        email = form.cleaned_data.get('email')
+        password = form.cleaned_data.get('password')
+        
+        user = User.objects.create_user(username, email, password)
+        if user:
+            login(request, user)
+            messages.success(request, 'Usuario creado exitosamente')
+            return redirect('index')
+        
+            
+        
     return render(request,'users/registro.html', {
         'form':form
     })
