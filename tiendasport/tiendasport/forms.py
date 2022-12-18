07bc4ae27b6d21e462b33d6595c2d2cb55611684
Claudio Caniullan <claudio.caniullan@usach.cl>
2022-12-18 00:35:30 -0300

    registro de usuarios con validaciones listo

--- tiendasport/tiendasport/forms.py
+++ tiendasport/tiendasport/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 
+from django.contrib.auth.models import User 
+
 class RegistroForm(forms.Form):
     username = forms.CharField(required=True,
                                min_length=4,
@@ -20,3 +22,39 @@ class RegistroForm(forms.Form):
                                    'class':'form-control',
                                    'id':'password',
                                }))
+    
+    password2 = forms.CharField(label='Confirmar password', 
+                                required=True,
+                                widget=forms.PasswordInput(attrs={
+                                    'class':'form-control'
+                                }))
+    
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        
+        if User.objects.filter(username=username).exists():
+            raise forms.ValidationError('El username ya s encuentra en uso')
+        
+        return username
+    
+    
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError('El email ya s encuentra en uso')
+        
+        return email
+    
+    def clean(self):
+        cleaned_data= super().clean()
+        
+        if cleaned_data.get('password2') != cleaned_data.get('password'):
+            self.add_error('password2', 'El password no coincide')
+        
+    
+    def save(self):
+        return User.objects.create_user(
+                self.cleaned_data.get('username'),
+                self.cleaned_data.get('email'),
+                self.cleaned_data.get('password'))
