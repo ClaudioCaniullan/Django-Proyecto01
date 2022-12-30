8581a62e09b6771cb06b8c3f9315c65dfa4f7acd
Claudio Caniullan <claudio.caniullan@usach.cl>
2022-12-30 01:07:27 -0300

    implementacion de slug producto para no exponer id del mismo

--- tiendasport/products/models.py
+++ tiendasport/products/models.py
@@ -7,7 +7,8 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=8, decimal_places=2, default=0.0)
     created_at = models.DateTimeField(auto_now_add=True)
-    
+    # solo obtendremos productos atravez de su slug
+    slug = models.SlugField(null=False, blank=False, unique=True)
     
     def __str__(self):
         return self.title
--- tiendasport/products/urls.py
+++ tiendasport/products/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('<pk>', views.ProductDetailView.as_view(), name='product') #id llave primaria, de esta form definimos ruta+id= /pk de un producto
+    path('<slug:slug>', views.ProductDetailView.as_view(), name='product') #id llave primaria, de esta form definimos ruta+slug= /slug de un producto
     
 ]
 
