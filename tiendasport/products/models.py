89d29302b413789e739d4929e2b93d9336ae45c2
Claudio Caniullan <claudio.caniullan@usach.cl>
2022-12-30 21:16:20 -0300

    nuevo generador de slug UNICOS automaticos mediante callbug set_slug, se ha modificado desde la linea 30 del archivo models.py de la app producto

--- tiendasport/products/models.py
+++ tiendasport/products/models.py
@@ -1,3 +1,6 @@
+
+import uuid
+
 from django.db import models
 from django.utils.text import slugify
 from django.db.models.signals import pre_save
@@ -26,7 +29,15 @@ class Product(models.Model):
     
 # callbug que se encarga de generar nuevos slug
 def set_slug(sender, instance, *args, **kwargs):
-    instance.slug = slugify(instance.title)
+    if instance.title and not instance.slug:
+        slug = slugify(instance.title)
+        
+        while Product.objects.filter(slug=slug).exists():
+            slug = slugify(
+                '{}-{}'.format(instance.title, str(uuid.uuid4())[:8])
+            )
+        
+        instance.slug = slug
  
 # registrar el callbug usando pre_save
 # le indicamos a Django que cuando antes que un objeto product se 
